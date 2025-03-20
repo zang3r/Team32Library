81a9b3209850929547b577b9e90ca2755db155c5
liston9 <142957646+liston9@users.noreply.github.com>
2025-03-19 19:25:46 -0600

    trying to fix gitignore

--- TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
+++ TestProject1/obj/Debug/net9.0/TestProject1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc3597d9cfa505e54da94ba43133bc4b5c37347")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
